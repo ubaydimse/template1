7d18252eb632e7c71de3363ebe35be2b23059bcd
ubaydimse <ubaydimse@gmail.com>
2019-09-02 19:49:34 +0700

    Add files via upload

---
+++ application/hooks/ssl.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class ssl
+{
+    function __construct()
+    {
+        // this is your constructor
+        parent::__construct();
+        $this->load->helper('form');
+        $this->load->helper('url');
+    }
+
+	function redirect_ssl() {
+		$CI =& get_instance();
+		$class = $CI->router->fetch_class();
+		$exclude =  array('client');  // add more controller name to exclude ssl.
+		if(!in_array($class,$exclude)) {
+			// redirecting to ssl.
+			$CI->config->config['base_url'] = str_replace('http://', 'https://', $CI->config->config['base_url']);
+			if ($_SERVER['SERVER_PORT'] != 443)redirect($CI->uri->uri_string());
+		} else {
+			// redirecting with no ssl.
+			$CI->config->config['base_url'] = str_replace('https://', 'http://', $CI->config->config['base_url']);
+			if ($_SERVER['SERVER_PORT'] == 443) redirect($CI->uri->uri_string());
+		}
+	}
+}
